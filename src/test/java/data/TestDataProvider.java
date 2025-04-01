21f715a459f584e6ce3698a93efc785d41ee1039
MariuszSolvd <mkaminski@solvd.com>
2025-04-01 16:42:49 +0200

    Data for 3rd test

--- src/test/java/data/TestDataProvider.java
+++ src/test/java/data/TestDataProvider.java
@@ -1,5 +1,6 @@
 package data;
 
+import com.solvd.utilis.models.CreateUser;
 import org.testng.annotations.DataProvider;
 
 public class TestDataProvider {
@@ -11,6 +12,14 @@ public class TestDataProvider {
                 {"7807888"},
                 {"7807880"},
                 {"7807884"}
+        };
+    }
+
+    @DataProvider
+    public Object[][] userProvider() {
+        return new Object[][] {
+                {new CreateUser("Arnold", "arnold@mail.com", "male", "active")},
+                {new CreateUser("Amanda", "amanda@mail.com", "female", "active")}
         };
     }
 }
