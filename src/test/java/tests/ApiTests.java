7cbd84a6c918ee5fecbe3d37493ced19f65a5bfb
MariuszSolvd <mkaminski@solvd.com>
2025-04-01 16:43:47 +0200

    3rd Test implementation

--- src/test/java/tests/ApiTests.java
+++ src/test/java/tests/ApiTests.java
@@ -1,7 +1,9 @@
 package tests;
 
 import com.solvd.utilis.ConfigReader;
+import com.solvd.utilis.models.CreateUser;
 import data.TestDataProvider;
+import io.restassured.http.ContentType;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -34,5 +36,21 @@ public class ApiTests {
         .then()
             .statusCode(200)
             .body("id", equalTo(Integer.parseInt(id)));
+    }
+
+    @Test(dataProvider = "userProvider", dataProviderClass = TestDataProvider.class)
+    public void createUser(CreateUser user) {
+        given()
+            .auth().oauth2(ConfigReader.get("token"))
+            .contentType(ContentType.JSON)
+            .body(user)
+        .when()
+            .post("users")
+        .then().log().body()
+            .statusCode(201)
+            .body("name", equalTo(user.name()))
+            .body("email", equalTo(user.email()))
+            .body("gender", equalTo(user.gender()))
+            .body("status", equalTo(user.status()));
     }
 }
